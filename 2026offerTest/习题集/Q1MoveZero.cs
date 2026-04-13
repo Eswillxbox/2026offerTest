8afe745f5a4de7c9fbf4dcf1457b13287267b2ee
EswillxNoah <yang17839773371@outlook.com>
2026-04-13 22:08:17 +0800

    Day1

---
+++ 2026offerTest/习题集/Q1MoveZero.cs
@@ -0,0 +1,31 @@
+﻿namespace _2026offerTest.习题集;
+
+public class Q1MoveZero 
+{
+    public void MoveZeroes(int[] nums) 
+    {
+        for(int i=0;i<=nums.Length-1;i++)
+        {
+            if(nums[i]==0)
+            {
+                int notZero=0;
+                int firstNum=0;
+                int firstIndex=0;
+                for(int j = i+1;j<=nums.Length-1;j++)
+                {
+                    if(nums[j]!=0)
+                    {
+                        notZero++;
+                        firstNum=nums[j];
+                        firstIndex=j;
+                        break;
+                    }
+                }
+                if(notZero==0)break;
+                int temp = nums[i];
+                nums[i] = firstNum;
+                nums[firstIndex] = temp;
+            }
+        }
+    }
+}
---
+++ 2026offerTest/习题集/Q2Searcha2DMatrix2.cs
@@ -0,0 +1,9 @@
+﻿namespace _2026offerTest.习题集;
+
+public class Q2Searcha2DMatrix2 
+{
+    public bool SearchMatrix(int[][] matrix, int target)
+    {
+        return true;
+    }
+}
