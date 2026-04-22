f72bb49d7a0708995c142681e3a8dcee2b1ffb0d
EswillxNoah <yang17839773371@outlook.com>
2026-04-22 21:22:01 +0800

    Day6

---
+++ 2026offerTest/习题集/Q3NextArrange.cs
@@ -0,0 +1,47 @@
+﻿namespace _2026offerTest.习题集;
+
+public class Q3NextArrange
+{
+    public void NextPermutation(int[] nums)
+    {
+        if (nums == null || nums.Length <= 1)
+            return;
+
+        int n = nums.Length;
+        int i = n - 2;
+
+        while (i >= 0 && nums[i] >= nums[i + 1])
+        {
+            i--;
+        }
+
+        if (i >= 0)
+        {
+            int j = n - 1;
+            while (j >= 0 && nums[j] <= nums[i])
+            {
+                j--;
+            }
+            Swap(nums, i, j);
+        }
+
+        Reverse(nums, i + 1, n - 1);
+    }
+
+    private void Swap(int[] nums, int i, int j)
+    {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+
+    private void Reverse(int[] nums, int left, int right)
+    {
+        while (left < right)
+        {
+            Swap(nums, left, right);
+            left++;
+            right--;
+        }
+    }
+}
